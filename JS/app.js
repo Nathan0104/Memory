866f6623dc62c1ee6692ba544f22deaa5afc1ecf
toulorg253 <nathan.toulorge@etu-unicaen.fr>
2026-09-23 10:53:13 +0200

    feat: core array data Logic and fisher-yates shuffle functional

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+let dimension = 150; 
+let imgStart = Math.floor(Math.random() * 100) + 1; 
+let cards = [];
+
+const images = []; 
+
+for (let i = imgStart; i <= imgStart + 7; i++) {
+    images.push(`https://picsum.photos/id/${i}/${dimension}/${dimension}`);
+}
+
+cards = [...images, ...images]; 
+
+function shuffle(array) {
+    for(let i = array.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1)); 
+
+        [array[i], array[j]] = [array[j], array[i]]; 
+    }
+}
